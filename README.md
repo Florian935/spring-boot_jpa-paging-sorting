0491d919f726d80d6916a85b3d5c2f1f4a4285ae
florian935 <florian.martin63000@gmail.com>
2021-08-26 23:32:55 +0200

    Feat: first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spring Boot App with JPA
+
+Purpose of this application is to demonstrate how pagination and sorting work with JPA.
---
+++ src/main/java/com/florian935/pagingsorting/PagingSortingApplication.java
@@ -0,0 +1,13 @@
+package com.florian935.pagingsorting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagingSortingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagingSortingApplication.class, args);
+	}
+
+}
